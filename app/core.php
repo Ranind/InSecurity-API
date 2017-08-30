08b977c5e3bbf28eaa875b5c44541678fe3a970a
cahenk <cahenk95@gmail.com>
2017-08-30 15:52:45 -0500

    php skeleton + webhook endpoint for deploying updates

---
+++ app/core.php
@@ -0,0 +1,64 @@
+<?php
+
+if(file_exists(__DIR__ . '/debug_mode')) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    ini_set('display_startup_errors', 'On');
+}
+
+use \Slim\Http\Request as Request;
+use \Slim\Http\Response as Response;
+
+require 'vendor/autoload.php';
+
+session_cache_limiter(false);
+session_start();
+
+// Create and configure slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+    'displayErrorDetails' => true,
+    'debug' => true,
+    'log' => [
+        'name' => 'api.insecurity.com',
+        'level' => Monolog\Logger::DEBUG,
+        'path' => __DIR__ . '/logs/app.log',
+    ]
+]];
+
+$app = new \Slim\App($config);
+
+$container = $app->getContainer();
+
+// Setup Monolog
+$container['log'] = function($c) {
+    $log = new \Monolog\Logger($c['settings']['log']['name']);
+    $fileHandler = new \Monolog\Handler\StreamHandler($c['settings']['log']['path'], $c['settings']['log']['level']);
+    $log->pushHandler($fileHandler);
+    return $log;
+};
+
+// Setup Database Connection
+$container['db'] = function ($c) {
+    $pdo = new PDO("sqlite:jenkins_admin.sqlite");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+
+
+
+// Github webhook proxy (for deployment hooks)
+$app->post('/update/{project}', function (Request $request, Response $response, $args) {
+
+    $data = $request->getParsedBody();
+
+    $cmd = escapeshellcmd(__DIR__ . '/get_updates.sh ' . $args['jobTitle']);
+
+    // Run the command and log output with timestamps
+    system("$cmd 2>&1 | while IFS= read -r line; do echo \"\$(date -u) \$line\"; done >> " . __DIR__ . "/logs/update.log");
+})->setName('update');
+
+
+$app->run();
--- get_updates.sh
+++ get_updates.sh
@@ -3,13 +3,21 @@
 API_WEB_ROOT="/var/www/api.insecurityscanner.com"
 ANG_WEB_ROOT="/var/www/insecurityscanner.com"
 
+API_NAME="api.insecurityscanner.com"
+FRONTEND_NAME="insecurityscanner.com"
+
 # API
-cd $API_WEB_ROOT
-git reset --hard [HEAD]
-git pull
-composer update
+if [ $# -eq 0 || $1 = $API_NAME ]; then
+    cd $API_WEB_ROOT
+    git reset --hard [HEAD]
+    git pull
+    rm debug_mode
+    composer update
+fi
 
 # Frontend
-cd $ANG_WEB_ROOT
-git reset --hard [HEAD]
-git pull
+if [ $# -eq 0 || $1 = $FRONTEND_NAME ]; then
+    cd $ANG_WEB_ROOT
+    git reset --hard [HEAD]
+    git pull
+fi
